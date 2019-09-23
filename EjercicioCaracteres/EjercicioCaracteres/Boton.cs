d8df2de75a79640d325fe3d157ea656901b756d2
DSaavedrag <dsaavedrag95@hotmail.com>
2019-09-23 11:19:09 -0300

    1.2

--- EjercicioCaracteres/EjercicioCaracteres/Boton.cs
+++ EjercicioCaracteres/EjercicioCaracteres/Boton.cs
@@ -1,12 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EjercicioCaracteres
 {
     class Boton
     {
+        private char[] caracteres;
+        private int numero;
+
+        public Boton(char[] caracteres, int numero)
+        {
+            this.caracteres = caracteres;
+            this.numero = numero;
+        }
+
+        public void setCaracteres(char[] caracteres)
+        {
+            this.caracteres = caracteres;
+        }
+
+        public char[] getCaracteres()
+        {
+            return this.caracteres;
+        }
+
+        public void setNumero(int numero)
+        {
+            this.numero = numero;
+        }
+
+        public int getNumero()
+        {
+            return this.numero;
+        }
+
+        public bool buscarChar(char c)
+        {
+            bool encontrado = false;
+            int i = 0;
+
+            while (encontrado == false && i < this.getCaracteres().Length)
+            {
+                if (this.getCaracteres()[i] == c)
+                {
+                    Console.WriteLine(this.getNumero());
+                    encontrado = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return false;
+        }
     }
 }
+
